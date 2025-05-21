7a3b926935b639acba6be0d12702fd7a601cee4d
K-lobiTo <calebam2112@gmail.com>
2025-05-20 18:56:30 -0600

    cnl snippet added

--- lua/klob/core/snippets.lua
+++ lua/klob/core/snippets.lua
@@ -8,29 +8,10 @@ local t = ls.text_node
 local i = ls.insert_node
 
 ls.add_snippets("all", {
-  s("ternary", {
-    -- Simple ternary snippet
-    t("{"),
-    i(1, "condition"),
-    t(" ? "),
-    i(2, "true"),
-    t(" : "),
-    i(3, "false"),
-    t("};"),
-  }),
-  s("forloop", {
-    -- For loop snippet
-    t("for (int "),
-    i(1, "i"),
-    t(" = 0; "),
-    i(1),
-    t(" < "),
-    i(2, "n"),
-    t("; "),
-    i(1),
-    t("++) {"),
-    t({"", "  "}),
-    i(0),
-    t({"", "}"}),
+  s("cnl", {
+      t("cout<<"),
+      i(1),
+      t("<<endl;"),
   }),
 })
+
