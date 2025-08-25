5dbbcf0d18abbd8df62da7793cb421b78c21a536
K-lobiTo <calebam2112@gmail.com>
2025-08-24 23:18:29 -0600

    Sparse Table snippet

--- lua/klob/core/snippets.lua
+++ lua/klob/core/snippets.lua
@@ -8,7 +8,7 @@ local t = ls.text_node
 local i = ls.insert_node
 local fmt = require("luasnip.extras.fmt").fmt
 
-ls.add_snippets("all", {
+ls.add_snippets("cpp", {
   s("cnl", {
       t("cout<<"),
       i(1),
@@ -50,9 +50,9 @@ ls.add_snippets("all", {
   -- Segment Tree
   s("segTree", fmt([[
     struct Mono {{
-        // TODO: define monoid type
+        // TODO: define monoid type & set neutral values
         // ll value;
-        // Mono(ll v = 0) : value(v) {{}} // set neutral values
+        // Mono(ll v = 0) : value(v) {{}}
     }};
 
     Mono operator+(Mono a, Mono b) {{
@@ -60,12 +60,12 @@ ls.add_snippets("all", {
         // return {{gcd(a.value, b.value)}};
     }}
 
-    struct Tree {{
+    struct SegmentTree {{
       int n;
       vector<Mono> s;
 
-      Tree(int n) : n(n), s(n<<1) {{}}
-      Tree(vector<Mono> const& a) : n(a.size()), s(a.size()<<1) {{
+      SegmentTree(int n) : n(n), s(n<<1) {{}}
+      SegmentTree(vector<Mono> const& a) : n(a.size()), s(a.size()<<1) {{
         copy(ALL(a), s.begin() + n);
         for (int i = n - 1; i > 0; --i) s[i] = s[i<<1] + s[i<<1|1];
       }}
@@ -74,7 +74,7 @@ ls.add_snippets("all", {
         for (s[x += n] = m; x >>= 1;) s[x] = s[x<<1] + s[x<<1|1];
       }}
 
-      Mono get(int l, int r) {{
+      Mono get(int l, int r) {{ // [l, r)
         Mono resl, resr;
         for (l += n, r += n; l < r; l >>= 1, r >>= 1) {{
           if (l&1) resl = resl + s[l++];
@@ -85,6 +85,45 @@ ls.add_snippets("all", {
     }};
   ]], {})),
 
+
+  -- Sparse Table
+  s("sparTable", fmt([[
+    template<typename T>
+    class SparseTable {{
+    private:
+        vector<vector<T>> st;
+        vector<int> log_table;
+        int n;
+
+    T op(T a, T b) {{
+        // TODO: define monoid (idempotent) operation
+        // return gcd(a, b);
+    }}
+        
+    public:
+        SparseTable(const vector<T>& arr) : n(arr.size()) {{
+            log_table.resize(n + 1);
+            log_table[1] = 0;
+            for (int i = 2; i <= n; i++) log_table[i] = log_table[i / 2] + 1;
+            
+            int k = log_table[n] + 1;
+            st.resize(n, vector<T>(k));
+            
+            for (int i = 0; i < n; i++) st[i][0] = arr[i];
+            
+            for (int j = 1; j < k; j++)
+                for (int i = 0; i + (1 << j) <= n; i++) 
+                    st[i][j] = op(st[i][j - 1], st[i + (1 << (j - 1))][j - 1]);
+        }}
+        
+        T query(int l, int r) {{ // [l, r]
+            int j = log_table[r - l + 1];
+            return op(st[l][j], st[r - (1 << j) + 1][j]);
+        }}
+    }};
+  ]], {})),
+
 }) 
+
 
 
