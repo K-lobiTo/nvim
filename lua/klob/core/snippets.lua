c56882fe70e464826f879c9a4336154b6953a739
K-lobiTo <calebam2112@gmail.com>
2025-10-04 17:51:44 -0600

    add United func in UnionFind snippet

--- lua/klob/core/snippets.lua
+++ lua/klob/core/snippets.lua
@@ -203,8 +203,12 @@ ls.add_snippets("cpp", {
         void init(int n) {{ uf.assign(n, -1); }}
         UnionFind(int n) {{ init(n); }}
         int Find(int i) {{ return (uf[i] == -1) ? i : uf[i] = Find(uf[i]); }}
+        bool United(int i, int j){{
+            return Find(i) == Find(j);
+        }}
+
         bool Union(int i, int j) {{
-            bool e = Find(i) == Find(j);
+            bool e = United(i, j);
             if (!e) uf[Find(i)] = Find(j);
             return e;
         }}
