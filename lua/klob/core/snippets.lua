61b0a0dc1ced10585880fa860e7ba96830788e76
K-lobiTo <calebam2112@gmail.com>
2025-10-04 17:37:10 -0600

    add Union Find snippet

--- lua/klob/core/snippets.lua
+++ lua/klob/core/snippets.lua
@@ -196,6 +196,19 @@ ls.add_snippets("cpp", {
         }}
     }};
   ]], {})),
+    -- Union Find
+  s("uF", fmt([[
+    struct UnionFind {{
+        vector<int> uf;
+        void init(int n) {{ uf.assign(n, -1); }}
+        int Find(int i) {{ return (uf[i] == -1) ? i : uf[i] = Find(uf[i]); }}
+        bool Union(int i, int j) {{
+            bool e = Find(i) == Find(j);
+            if (!e) uf[Find(i)] = Find(j);
+            return e;
+        }}
+    }};
+  ]], {})),
 
 }) 
 
