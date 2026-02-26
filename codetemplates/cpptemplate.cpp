3156ae31f909f29be089f827d92c41f9d58ae060
K-lobiTo <calebam2112@gmail.com>
2026-02-25 19:07:25 -0600

    update template

--- codetemplates/cpptemplate.cpp
+++ codetemplates/cpptemplate.cpp
@@ -2,11 +2,9 @@
 using namespace std;
   
 #define DEBUG(n) cout<<#n<<" = "<<n<< endl
-#define rAuto(arr) for(auto &e:arr)cin>>e
-#define ALL(v) (v).begin(), (v).end()
-#define rALL(v) (v).rbegin(), (v).rend()
 #define vec vector
 #define ll long long
+
 const int MAX = 2e5+20, MOD = 1e9+7;
 int t=1;
   
@@ -14,8 +12,8 @@ void solve(){
 }
   
 int32_t main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0); 
+    ios::sync_with_stdio(0), cin.tie(0); 
+
     cin>>t;
     while(t--){
         solve();
