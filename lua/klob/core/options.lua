1ae858c4b76d23cea676bea483212dbe311adfff
K-lobiTo <calebam2112@gmail.com>
2026-02-25 20:33:16 -0600

    fix: clipboard access in Hyprland/Wayland environment

--- lua/klob/core/options.lua
+++ lua/klob/core/options.lua
@@ -9,22 +9,34 @@ opt.shiftwidth = 4
 opt.expandtab = true
 opt.autoindent = true
 
-vim.api.nvim_create_autocmd("VimEnter", {
-  pattern = "*.cpp",
-  callback = function()
-    if vim.fn.filereadable("test") == 0 then
-        local clipboard_content = vim.fn.systemlist('xclip -o -selection clipboard')
-        if #clipboard_content > 0 then
-            vim.fn.writefile(clipboard_content, "test")
-        else
-            vim.fn.writefile({}, "test")
-        end
-    end
 
-    vim.cmd("vsplit test | wincmd l")
-    vim.cmd("wincmd H")
-    vim.cmd("wincmd l")
-    vim.cmd("wincmd h")
-    vim.bo.bufhidden = "hide"   -- Keep buffer when hidden
-  end
+local function get_clipboard_content()
+    if os.getenv("WAYLAND_DISPLAY") then
+        -- Wayland
+        return vim.fn.systemlist('wl-paste')
+    else
+        -- X11
+        return vim.fn.systemlist('xclip -o -selection clipboard 2>/dev/null')
+    end
+end
+
+vim.api.nvim_create_autocmd("VimEnter", {
+    pattern = "*.cpp",
+    callback = function()
+        if vim.fn.filereadable("test") == 0 then
+            local clipboard_content = get_clipboard_content()
+            if #clipboard_content > 0 and clipboard_content[1] ~= "" then
+                vim.fn.writefile(clipboard_content, "test")
+            else
+                vim.fn.writefile({}, "test")
+            end
+        end
+
+        vim.cmd("vsplit test | wincmd l")
+        vim.cmd("wincmd H")
+        vim.cmd("wincmd l")
+        vim.cmd("wincmd h")
+        vim.bo.bufhidden = "hide"
+    end
 })
+
